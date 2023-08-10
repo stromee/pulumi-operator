c5745c71260028fd983dc4f5fe3686c993e1538f
Leonard Seibold <leo@zrtx.de>
2023-08-10 22:12:43 +0200

    handle async stuff

--- pulumi-operator-kubernetes-job/src/git/service.rs
+++ pulumi-operator-kubernetes-job/src/git/service.rs
@@ -1,19 +1,26 @@
-use std::path::PathBuf;
+use std::{collections::BTreeMap, path::PathBuf};
 
-use git2::Cred;
-use k8s_openapi::api::core::v1::Secret;
+use git2::{Cred, RemoteCallbacks};
+use k8s_openapi::{api::core::v1::Secret, ByteString};
 use kube::core::ObjectMeta;
 use pulumi_operator_base::Inst;
 use pulumi_operator_kubernetes::{
+  config_provider::{ConfigError, ConfigProvider},
   kubernetes::service::KubernetesService,
-  stack::source::git::inner::{InnerGitStackSourceSpec, GitAuthType},
+  stack::source::git::inner::{GitAuth, GitAuthType, InnerGitStackSourceSpec},
 };
 use springtime_di::Component;
 use thiserror::Error;
+use tokio::{
+  runtime::Builder,
+  sync::{mpsc, oneshot},
+  task::LocalSet,
+};
 
 #[derive(Component)]
 pub struct GitService {
   kubernetes_service: Inst<KubernetesService>,
+  config_provider: Inst<ConfigProvider>,
 }
 
 #[derive(Debug, Error)]
@@ -29,6 +36,18 @@ pub enum GitError {
 
   #[error("Git error: {0}")]
   Git(#[from] git2::Error),
+
+  #[error("Config error: {0}")]
+  Config(#[from] ConfigError),
+
+  #[error("Failed to join git fetch task: {0}")]
+  Join(#[from] tokio::task::JoinError),
+
+  #[error("Failed to build runtime for git fetch: {0}")]
+  Runtime(#[from] tokio::io::Error),
+
+  #[error("Failed to communicate between threads: {0}")]
+  Recv(#[from] oneshot::error::RecvError),
 }
 
 impl GitService {
@@ -37,31 +56,121 @@ impl GitService {
     spec: &InnerGitStackSourceSpec,
     metadata: &ObjectMeta,
   ) -> Result<impl Into<PathBuf>, GitError> {
-
-    let cred = match spec.auth.as_ref() {
-      Some(auth) => {
-        let namespace = metadata.namespace.clone().map;
-        let secret = self.kubernetes_service.get_in_namespace::<Secret>(namespace, &auth.secret_ref).await?;
-
-        Some(match auth.kind {
-          GitAuthType::Ssh => {
-           
-
-            todo!()
-          }
-          GitAuthType::Basic => {
-            let data = secret.data.ok_or_else(|| GitError::DataEmpty)?;
-            let username = data.get("username").ok_or_else(|| GitError::DataEmpty)?.clone();
-            let username = String::from_utf8(username.0)?;
-            let password = data.get("password").ok_or_else(|| GitError::DataEmpty)?.clone();
-            let password = String::from_utf8(password.0)?;
-            Cred::userpass_plaintext(&username, &password)?
-          }
-        })    
-      },
-      None => None,
+    let namespace = match &metadata.namespace {
+      Some(ns) => ns.clone(),
+      None => self.config_provider.operator_namespace()?,
     };
-    Ok("/")
+
+    let git_controller = GitController {
+      kubernetes_service: self.kubernetes_service.clone(),
+    };
+    let spec = spec.clone();
+
+    let (tx, rx) = oneshot::channel();
+    let rt = Builder::new_current_thread().enable_all().build()?;
+
+    std::thread::spawn(move || {
+      let local = LocalSet::new();
+
+      local.spawn_local(async move {
+        let res =  async move {
+          let mut callback = RemoteCallbacks::new();
+
+          if let Some(auth) = &spec.auth {
+            let data = git_controller.get_secret(&namespace, &auth).await?;
+            git_controller.build_callback(auth, &data, &mut callback)?;
+          }
+
+          Ok::<&str, GitError>("/")
+        }.await;
+        tx.send(res).expect("Failed to communicate between threads.");
+      });
+
+      rt.block_on(local);
+    });
+    rx.await?
+  }
+}
+
+struct GitController {
+  kubernetes_service: Inst<KubernetesService>,
+}
+
+impl GitController {
+  async fn get_secret(
+    &self,
+    namespace: &str,
+    auth: &GitAuth,
+  ) -> Result<BTreeMap<String, ByteString>, GitError> {
+    let secret = self
+      .kubernetes_service
+      .get_in_namespace::<Secret>(namespace, &auth.secret_ref)
+      .await?;
+
+    let data = secret.data.ok_or_else(|| GitError::DataEmpty)?;
+
+    Ok(data)
   }
 
+  fn build_callback(
+    &self,
+    auth: &GitAuth,
+    data: &BTreeMap<String, ByteString>,
+    callback: &mut RemoteCallbacks<'_>,
+  ) -> Result<(), GitError> {
+    let fallback_username = match data.get("username") {
+      Some(username) => String::from_utf8(username.clone().0)?,
+      None => "git".into(),
+    };
+
+    Some(match auth.kind {
+      GitAuthType::Ssh => {
+        let publickey = match data.get("publickey") {
+          Some(publickey) => Some(String::from_utf8(publickey.clone().0)?),
+          None => None,
+        };
+
+        let privatekey = String::from_utf8(
+          data
+            .get("privatekey")
+            .ok_or_else(|| GitError::DataEmpty)?
+            .clone()
+            .0,
+        )?;
+
+        let passphrase = match data.get("passphrase") {
+          Some(passphrase) => Some(String::from_utf8(passphrase.clone().0)?),
+          None => None,
+        };
+
+        callback.credentials(move |_url, username_from_url, _allowed_types| {
+          Cred::ssh_key_from_memory(
+            username_from_url.unwrap_or(&fallback_username),
+            publickey.as_deref(),
+            &privatekey,
+            passphrase.as_deref(),
+          )
+        });
+      }
+      GitAuthType::Basic => {
+        let username = data
+          .get("username")
+          .ok_or_else(|| GitError::DataEmpty)?
+          .clone();
+        let username = String::from_utf8(username.0)?;
+        let password = data
+          .get("password")
+          .ok_or_else(|| GitError::DataEmpty)?
+          .clone();
+        let password = String::from_utf8(password.0)?;
+
+        callback.credentials(
+          move |_url, _username_from_url, _allowed_types| {
+            Cred::userpass_plaintext(&username, &password)
+          },
+        );
+      }
+    });
+    Ok(())
+  }
 }
