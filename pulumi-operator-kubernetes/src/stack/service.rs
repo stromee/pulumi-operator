333d727e087e2614d58ceb40b5fdf3bf2d909369
Jan Brachthäuser <contact@jan-br.de>
2023-09-18 01:04:23 +0200

    Update pulumi command path and improve Docker configuration
    Change the path for the 'pulumi' command in the Pulumi CLI lib from '/root/.pulumi/bin/pulumi' to 'pulumi' to enable better path lookup. Also, added 'npm' as an installed package in the Dockerfile for the pulumi-operator-kubernetes-job improving container build. The image for pulumi-operator-kubernetes-job in the Kubernetes Service Stack was updated to 1.0.10.
    
    This refactoring improves the robustness of the code and ensures that the container has all necessities for execution.

--- pulumi-operator-kubernetes/src/stack/service.rs
+++ pulumi-operator-kubernetes/src/stack/service.rs
@@ -53,7 +53,7 @@ impl PulumiStackService for KubernetesPulumiStackService {
                 "spec": {
                     "containers": [{
                         "name": "pulumi",
-                        "image": "ghcr.io/stromee/pulumi-operator/pulumi-operator-kubernetes-job:1.0.9",
+                        "image": "ghcr.io/stromee/pulumi-operator/pulumi-operator-kubernetes-job:1.0.10",
                         "env": [{
                             "name": "PULUMI_STACK",
                             "value": name
